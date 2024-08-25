32c3523d126ed3d6f8ace217d167eeb78b568081
Srikant Gudi <srikantgudi@gmail.com>
2024-08-25 23:30:53 +0530

    Initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,2 @@
-<script>
-</script>
-<div class="grid md:grid-cols-2 sm:grid-cols-1 gap-8 m-8">
-  <a class="rounded-md bg-gray-300 p-8 text-center text-2xl hover:bg-blue-400" href="/calendars">Calendars</a>
-  <a class="rounded-md bg-gray-300 p-8 text-center text-2xl hover:bg-blue-400" href="/clocks">Clocks</a>
-  <a class="rounded-md bg-gray-300 p-8 text-center text-2xl hover:bg-blue-400" href="/storedemo">Store</a>
-  <a class="rounded-md bg-gray-300 p-8 text-center text-2xl hover:bg-blue-400" href="/dynamicdata">Dynamic Data</a>
-</div>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
--- src/routes/api/comments/+server.js
+++
@@ -1,7 +0,0 @@
-import { json } from "@sveltejs/kit";
-
-export async function GET() {
-  const resp = await fetch("https://jsonplaceholder.typicode.com/comments");
-  const data = await resp.json();
-  return json(data);
-}
